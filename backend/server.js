c5227c18ab8873765cec180cec87ea52677fa52f
drewb73 <bartondrew5@gmail.com>
2022-05-17 09:36:36 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
-const PORT = 5000
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to the Support Desk Api'})
+})
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT))
