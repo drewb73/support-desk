e1e8654dd9c3c9cf6d8879f22412b096d018c549
drewb73 <bartondrew5@gmail.com>
2022-05-21 10:40:19 -0700

    finish login and home pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
+import {useState} from 'react'
+import {toast} from 'react-toastify'
+import {FaSignInAlt} from 'react-icons/fa'
+
 
 function Login() {
+    // create state for form
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    // destructure from form data objects in state
+    const {email, password} = formData
+
+    // create onChange event function
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }))
+    }
+
+    // create onSubmit function for form and check if password and password2 equal each other
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+
+     
+    }
+
+
+
+
   return (
-    <div>
-        <h1>Login</h1>
-    </div>
+    <>
+        <section className='heading'>
+            <h1>
+                <FaSignInAlt /> Login
+            </h1>
+            <p>Please login to get support</p>
+        </section>
+
+        <section className='form'>
+            <form onSubmit={onSubmit}>
+                <div className='form-group'>
+                    <input type='email' className='form-control' id='email' value={email} onChange={onChange} name='email' placeholder='Enter Your Email' required />
+                </div>
+                <div className='form-group'>
+                    <input type='password' className='form-control' id='password' value={password} onChange={onChange} name='password' placeholder='Create Password' required />
+                </div>
+                <div className='form-group'>
+                    <button className='btn btn-block'>Submit</button>
+                </div>
+            </form>
+        </section>
+    </>
   )
 }
 
